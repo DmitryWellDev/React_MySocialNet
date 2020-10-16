3249b3fa96a91e71b5b613f5d627ba388dcdf1f6
DmitryWellDew <dewwell0001@gmail.com>
2020-10-16 22:32:46 +0300

    Updates

---
+++ precache-manifest.fec387dd313ba506faaff96f429174b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6740e64818fbf483c8ccd26b3488a7b3",
+    "url": "/React_wayOf-SAMURAI/index.html"
+  },
+  {
+    "revision": "a3c2e3b105db5e0b6691",
+    "url": "/React_wayOf-SAMURAI/static/css/main.aae513ef.chunk.css"
+  },
+  {
+    "revision": "2517103bd2148ed8cc05",
+    "url": "/React_wayOf-SAMURAI/static/js/2.5f582b90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_wayOf-SAMURAI/static/js/2.5f582b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c2e3b105db5e0b6691",
+    "url": "/React_wayOf-SAMURAI/static/js/main.bc567455.chunk.js"
+  },
+  {
+    "revision": "e660be9b624421e68abe",
+    "url": "/React_wayOf-SAMURAI/static/js/runtime-main.a8b49d38.js"
+  }
+]);
