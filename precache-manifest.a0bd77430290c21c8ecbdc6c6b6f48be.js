2e143cc41977e50757ff4b9f6561e685f9f91a4c
DmitryWellDew <dewwell0001@gmail.com>
2021-01-11 21:41:01 +0300

    Updates

---
+++ precache-manifest.a0bd77430290c21c8ecbdc6c6b6f48be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5986df063fe64286aee756c8d1a3bf",
+    "url": "/React_MySocialNet/index.html"
+  },
+  {
+    "revision": "3cbe9d604d3a58a079c7",
+    "url": "/React_MySocialNet/static/css/main.061d55b0.chunk.css"
+  },
+  {
+    "revision": "c8fc617b9e93061ecfd8",
+    "url": "/React_MySocialNet/static/js/2.e4d3dc87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_MySocialNet/static/js/2.e4d3dc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbe9d604d3a58a079c7",
+    "url": "/React_MySocialNet/static/js/main.0481999a.chunk.js"
+  },
+  {
+    "revision": "40c5d6f573ab694c0b02",
+    "url": "/React_MySocialNet/static/js/runtime-main.09afcd4a.js"
+  },
+  {
+    "revision": "b3f758f8f3a704d16794abddfaec7ccf",
+    "url": "/React_MySocialNet/static/media/50-Beautiful-and-Minimalist-Presentation-Backgrounds-01.b3f758f8.jpg"
+  },
+  {
+    "revision": "aa47ede4bc9ed6bbea9e3f61ba500a00",
+    "url": "/React_MySocialNet/static/media/d1151f5f321f859c4d13f28cb71cf424_w200.aa47ede4.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_wayOf-SAMURAI/precache-manifest.fec387dd313ba506faaff96f429174b0.js"
+  "/React_MySocialNet/precache-manifest.a0bd77430290c21c8ecbdc6c6b6f48be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_wayOf-SAMURAI/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_MySocialNet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
